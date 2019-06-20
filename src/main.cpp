bc137410d4a48f72cf7741db2e7188b9a90bb206
KARTES25 <stilalex@yandex.ru>
2019-06-20 15:21:18 +0700

    add code in main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,12 @@
 #include "Calculater.h"
 #include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+
